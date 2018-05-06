8ba013f558b666ca801bb45f18d429c504bc2bfd
Marcos Rodrigues <m.ferreira.geraldo@avanade.com>
2018-05-06 15:41:22 -0300

    continuidade de dev..

--- src/app/components/shared/divisor/divisor.component.ts
+++ src/app/components/shared/divisor/divisor.component.ts
@@ -9,6 +9,8 @@ export class DivisorComponent implements OnInit {
   @Input() width: string;
   @Input() height: string;
   @Input() marginTop: string;
+  @Input() color: string = '#b7dab9';
+  @Input() marginBottom: string = '';
 
   constructor() { }
 
